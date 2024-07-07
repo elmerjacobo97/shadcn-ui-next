e520eda16b4da9c4eadc13c2bb0134b9d7d1e4a1
Elmer Jacobo Otiniano <88252912+elmerjacobo97@users.noreply.github.com>
2024-07-07 18:54:41 -0500

    progress

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -21,6 +21,7 @@ const links = [
   { name: 'context-menu', href: 'context-menu' },
   { name: 'menu-bar', href: 'menu-bar' },
   { name: 'input-otp', href: 'input-otp' }, // One-time password (OTP)
+  { name: 'progress', href: 'progress' },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
