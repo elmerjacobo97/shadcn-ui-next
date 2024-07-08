9e269ca0caad62c461d46b1279e8a4134ba33b2b
Elmer Jacobo Otiniano <88252912+elmerjacobo97@users.noreply.github.com>
2024-07-07 21:37:24 -0500

    toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { Toaster as SonnerToaster } from '@/components/ui/sonner';
+import { Toaster } from '@/components/ui/toaster';
+
 import './globals.css';
 
 const fontSans = FontSans({
@@ -24,6 +26,7 @@ export default function RootLayout({
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         {children}
         <SonnerToaster richColors />
+        <Toaster />
       </body>
     </html>
   );
